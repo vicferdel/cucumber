40acd8f955ec7da440d39284ca058552c3f15fc3
Victor <vicferdel@gmail.com>
2015-03-10 00:25:58 +0100

    Importe inicial

---
+++ CumcuberEjemplos/src/main/java/es/pruebas/bussines/Calculadora.java
@@ -0,0 +1,17 @@
+package es.pruebas.bussines;
+
+public class Calculadora{
+
+	public int sumar(int a, int b) {
+		return a+b;
+	}
+	
+	public int restar(int a,int b){
+		
+		
+		return a-b;
+	}
+	
+	
+	
+}
---
+++ CumcuberEjemplos/src/test/java/es/pruebas/steps/CalculadoraSteps.java
@@ -0,0 +1,29 @@
+package es.pruebas.steps;
+
+import org.junit.Assert;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import es.pruebas.bussines.Calculadora;
+
+public class CalculadoraSteps {
+private Calculadora miCalculadora;
+	
+	private int resultadoSuma;
+
+	@Given("^Encendemos la calculadora$")
+	public void encendemos_la_calculadora() throws Throwable {
+	   miCalculadora=new Calculadora();
+	}
+
+	@When("^Sumamos los valores (\\d+) y (\\d+)$")
+	public void sumamos_los_valores_y(int arg1, int arg2) throws Throwable {
+		resultadoSuma=miCalculadora.sumar(arg1, arg2);
+	}
+
+	@Then("^El resultado final es (\\d+)$")
+	public void el_resultado_final_es(int arg1) throws Throwable {
+	    Assert.assertEquals(5, resultadoSuma);
+	}
+}
