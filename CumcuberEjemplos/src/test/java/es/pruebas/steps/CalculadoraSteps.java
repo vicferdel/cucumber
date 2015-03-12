5f9d21ff772bc87bb69c429ebf3edc293e87dbad
Victor <vicferdel@gmail.com>
2015-03-12 22:03:27 +0100

    Cambios metodos

--- CumcuberEjemplos/src/test/java/es/pruebas/steps/CalculadoraSteps.java
+++ CumcuberEjemplos/src/test/java/es/pruebas/steps/CalculadoraSteps.java
@@ -13,17 +13,17 @@ private Calculadora miCalculadora;
 	private int resultadoSuma;
 
 	@Given("^Encendemos la calculadora$")
-	public void encendemos_la_calculadora() throws Throwable {
+	public void iniciarCalculadora() throws Throwable {
 	   miCalculadora=new Calculadora();
 	}
 
 	@When("^Sumamos los valores (\\d+) y (\\d+)$")
-	public void sumamos_los_valores_y(int arg1, int arg2) throws Throwable {
+	public void testSuma(int arg1, int arg2) throws Throwable {
 		resultadoSuma=miCalculadora.sumar(arg1, arg2);
 	}
 
 	@Then("^El resultado final es (\\d+)$")
-	public void el_resultado_final_es(int arg1) throws Throwable {
+	public void testResultado(int arg1) throws Throwable {
 	    Assert.assertEquals(arg1, resultadoSuma);
 	}
 }
