ecea6a7b159be48c529b7056b7d49f8fc7eda29c
Victor <vicferdel@gmail.com>
2015-03-12 22:30:08 +0100

    Cambios

--- CumcuberEjemplos/src/test/java/es/pruebas/steps/CalculadoraSteps.java
+++ CumcuberEjemplos/src/test/java/es/pruebas/steps/CalculadoraSteps.java
@@ -11,6 +11,8 @@ public class CalculadoraSteps {
 private Calculadora miCalculadora;
 	
 	private int resultadoSuma;
+	private int operando1;
+	private int operando2;
 
 	@Given("^Encendemos la calculadora$")
 	public void iniciarCalculadora() throws Throwable {
@@ -19,11 +21,14 @@ private Calculadora miCalculadora;
 
 	@When("^Sumamos los valores (\\d+) y (\\d+)$")
 	public void testSuma(int arg1, int arg2) throws Throwable {
-		resultadoSuma=miCalculadora.sumar(arg1, arg2);
+		operando1=arg1;
+		operando2=arg2;
+		resultadoSuma=miCalculadora.sumar(operando1, operando2);
 	}
 
 	@Then("^El resultado final es (\\d+)$")
 	public void testResultado(int arg1) throws Throwable {
-	    Assert.assertEquals(arg1, resultadoSuma);
+	    System.out.println("Resultado  "+operando1+" + "+operando2+" = "+resultadoSuma);
+		Assert.assertEquals(arg1, resultadoSuma);
 	}
 }
